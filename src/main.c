89a6527f9dcb5ab38c2f3b8b56997e14130a4035
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2021-07-11 13:51:45 +0900

    :sparkles: Feature : ファイル指定していない場合のエラーを追加

--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
 #include "../include/common.h"
+#include <stdio.h>
 
-int main(){
+int main(int argc, char *argv[]){
+	if(argc < 2){
+		printf("\x1b[31mERROR: Missing command line argument.\n");
+		return 0;
+	}
+
 	lexical_analysis();
 
 	return 0;
